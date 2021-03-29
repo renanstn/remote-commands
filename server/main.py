cb17a4875815565f604432ee8e8185a4119b3ecd
Renan Santana <renan_ota@hotmail.com>
2021-03-29 17:59:51 -0300

    Adiciona comando lock_screen

--- server/main.py
+++ server/main.py
@@ -6,10 +6,24 @@ app = Flask(__name__)
 
 
 @app.route('/shortcut', methods=['POST'])
-def hello_world():
+def shortcut():
     data = request.get_json()
+
+    if not data.get('command', False):
+        return {
+            "success": False,
+            "message": "send command name in 'command' field"
+        }
+
     if data.get('command') == 'minimize_all':
         keyboard.press_and_release('windows+d')
+
+    elif data.get('command') == 'lock_screen':
+        keyboard.press_and_release('windows+l')
+
+    else:
+        return {"success": False, "message": "command not found"}
+
     return {"success": True}
 
 
