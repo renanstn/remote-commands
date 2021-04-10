6fb07a67178de8422ef5e338a1958316dd1f26ac
Renan Santana <renan_ota@hotmail.com>
2021-04-09 22:16:10 -0300

    Implementa banco e flask admin

--- server/main.py
+++ server/main.py
@@ -1,32 +1,54 @@
 import keyboard
 from flask import Flask, request
+from peewee import SqliteDatabase, AutoField, IntegerField, CharField, Model
+from flask_admin import Admin
+from flask_admin.contrib.peewee import ModelView
 
+
+DEBUG = True
+SECRET_KEY = 'secret'
+FLASK_ADMIN_SWATCH = 'darkly'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+database =SqliteDatabase('database.db')
+admin = Admin(app, name='remote-commands', template_mode='bootstrap3')
 
+
+class Command(Model):
+    id = AutoField(primary_key=True)
+    index = IntegerField()
+    command = CharField(max_length=255)
+
+    class Meta:
+        database = database
+
+
+admin.add_view(ModelView(Command, database, 'Commands'))
 
 @app.route('/shortcut', methods=['POST'])
 def shortcut():
     data = request.get_json()
-
     if not data.get('command', False):
         return {
             "success": False,
             "message": "send command name in 'command' field"
         }
-
     if data.get('command') == 'minimize_all':
         keyboard.press('windows+d')
-
     else:
         return {"success": False, "message": "command not found"}
-
     return {"success": True}
 
 
-@app.route('/command/<int:command>')
-def command(command):
+@app.route('/command/<int:command_index>')
+def exec_command(command_index):
+    to_exec = Command.get(index=command_index)
+    system(to_exec.command)
+    print(to_exec.command)
     return {"success": True}
 
 
-app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    Command.create_table()
+    app.run(host='0.0.0.0')
