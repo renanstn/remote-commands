8ce28397ec5a65b0a8193371b85eae07cc1c1ffb
Renan Santana <renan_ota@hotmail.com>
2022-01-31 23:37:02 -0300

    format files using black

--- server/main.py
+++ server/main.py
@@ -13,17 +13,17 @@ from models import Command, Clipbullet
 from settings import *
 
 
-chime.theme('mario')
+chime.theme("mario")
 app = Flask(__name__)
 app.config.from_object(__name__)
-admin = Admin(app, name='remote-commands', template_mode='bootstrap3')
+admin = Admin(app, name="remote-commands", template_mode="bootstrap3")
 
 # Add views to flask admin
 admin.add_view(ModelView(Command))
 admin.add_view(ModelView(Clipbullet))
 
 # Define endpoints
-@app.route('/shortcut', methods=['POST'])
+@app.route("/shortcut", methods=["POST"])
 def shortcut():
     """
     Expects a JSON containing the shortcut name in 'shortcut' field
@@ -32,20 +32,18 @@ def shortcut():
     - 'mute_unmute_meet'
     """
     data = request.get_json()
-    if not data.get('shortcut', False):
-        return {
-            "success": False,
-            "message": "Send shortcut name in 'shortcut' field"
-        }
-    if data.get('shortcut') == 'minimize_all':
-        keyboard.press_and_release('windows+d')
-    elif data.get('shortcut') == 'mute_unmute_meet':
-        keyboard.press_and_release('ctrl+d')
+    if not data.get("shortcut", False):
+        return {"success": False, "message": "Send shortcut name in 'shortcut' field"}
+    if data.get("shortcut") == "minimize_all":
+        keyboard.press_and_release("windows+d")
+    elif data.get("shortcut") == "mute_unmute_meet":
+        keyboard.press_and_release("ctrl+d")
     else:
         return {"success": False, "message": "Shortcut not found"}
     return {"success": True}
 
-@app.route('/command/<int:command_id>', methods=['POST'])
+
+@app.route("/command/<int:command_id>", methods=["POST"])
 def exec_command(command_id):
     """
     Exec a previously created command in terminal
@@ -55,10 +53,7 @@ def exec_command(command_id):
         to_exec = Command.get(id=command_id)
         command = to_exec.command
     except DoesNotExist:
-        return {
-            "success": False,
-            "message": f"Command not found: ID {command_id}"
-        }
+        return {"success": False, "message": f"Command not found: ID {command_id}"}
 
     # Executa o comando
     output = os.system(command)
@@ -71,7 +66,8 @@ def exec_command(command_id):
         chime.error()
         return {"command": command, "success": False}
 
-@app.route('/clipbullet/<int:paste_id>', methods=['POST'])
+
+@app.route("/clipbullet/<int:paste_id>", methods=["POST"])
 def load_clipbullet(paste_id):
     """
     Loads a previously created text to clipboard
@@ -82,20 +78,17 @@ def load_clipbullet(paste_id):
         clipboard.copy(to_copy.text)
         chime.info()
     except DoesNotExist:
-        return {
-            "success": False,
-            "message": f"Text not found: ID {paste_id}"
-        }
+        return {"success": False, "message": f"Text not found: ID {paste_id}"}
     return {"success": True}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     Command.create_table()
     Clipbullet.create_table()
     # Obter o IP local da máquina, apenas para exibição
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(('8.8.8.8', 1))
+    s.connect(("8.8.8.8", 1))
     local_ip = s.getsockname()[0]
     print(f" * Running app on: {local_ip}")
     print(f" * Add commands on: http://{local_ip}:5000/admin")
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
